0c1c104933d76a700f7f72767b31798914efa876
Tristan Collicott <tristancollicott@gmail.com>
2020-08-22 15:30:37 -0400

    - added flag to print as columns - changed parseWidth to inlinePrint

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ const blueLog = text => console.log(chalk.rgb(173, 235, 235)(text));
 const pinkLog = text => console.log(chalk.rgb(235, 173, 235)(text));
 const redLog = text => console.log(chalk.rgb(242, 56, 56)(text));
 
-program.version('0.4.0');
+program.version('0.5.0');
 program
 	.option('-d, --dir','prints directories (cannot be used with -f)')
 	.option('-f, --file','prints files (cannot be used with -d)')
-	.option('-s, --size','prints file sizes')
+	.option('-s, --size','prints file sizes (cannot be used with -d)')
+	.option('-c, --columns','prints as one or two columns')
 	.parse(process.argv);
 
-if (program.file && program.dir) { redlog("Invalid argument combination\nTry ls -h for more help"); return }
+if (program.file || program.columns && program.dir) { redLog(`Invalid argument combination: ${process.argv.slice(2)}\nTry ls -h for more help`); return }
 
 // Finds where to add spaces to make sure file/folder names aren't cut off
 let terminalWidth = process.stdout.columns;
-const parseWidth = files => {
+const inlinePrint = files => {
 	let output = files.join(' ');
 	let count = 1;
 	let lines;
@@ -72,11 +73,29 @@ const readDirectory = async () => {
 	else if (!folders.length && program.dir) { whiteLog('No Folders, try removing flag'); }
 	else if (!files.length && program.file) { whiteLog('No Files, try removing flag'); }
 
-	if (!program.file && folders.length) {
-		blueLog(parseWidth(folders));
-	}
-	if (!program.dir && files.length) {
-		if (folders.length && !program.file) { console.log('') }
-		pinkLog(parseWidth(files));
+	if (program.columns) {
+		if (program.dir) {
+			whiteLog("Folders:");
+			folders.forEach(folder => blueLog(folder));
+		} else if (program.file) {
+			whiteLog("Files:");
+			files.forEach(file => pinkLog(file));
+		} else {
+			whiteLog("Folders:");
+			folders.forEach(folder => blueLog(folder));
+			console.log('');
+			whiteLog("Files:");
+			files.forEach(file => pinkLog(file));
+		}
+	} else {
+		if (!program.file && folders.length) {
+			whiteLog("Folders:");
+			blueLog(inlinePrint(folders));
+		}
+		if (!program.dir && files.length) {
+			if (folders.length && !program.file && !program.columns) { console.log('') }
+			whiteLog("Files:");
+			pinkLog(inlinePrint(files));
+		}
 	}
 })();
